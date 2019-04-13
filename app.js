b4313c96859dbdbd372cb908374d420b967e86ee
walle <michelle210181@gmail.com>
2019-04-13 14:51:31 +0800

    Add edit page

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const exphbs = require("express-handlebars");
-const restaurantList = require("./restaurant.json");
+const restaurantList = require("./models/seeds/restaurant.json");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 mongoose.connect("mongodb://localhost/restaurant", { useNewUrlParser: true });
 const db = mongoose.connection;
@@ -23,27 +24,95 @@ app.set("view engine", "handlebars"); //告訴 Express 說要設定的 view engi
 
 // setting static files
 app.use(express.static("public")); // 告訴 Express 靜態檔案的資料夾位置
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const Restaurant = require("./models/restaurant");
 
 // routes setting
+
+//瀏覽所有餐廳
 app.get("/", (req, res) => {
-  res.render("index", { restaurants: restaurantList.results });
+  Restaurant.find((err, restaurants) => {
+    if (err) return console.error(err);
+    return res.render("index", { restaurants: restaurants });
+  });
 });
 
-app.get("/restaurants/:store_id", (req, res) => {
-  const store = restaurantList.results.filter(
-    store => Number(store.id) === Number(req.params.store_id)
-  );
-  res.render("show", { store: store[0] });
+//瀏覽其中一個餐廳的詳細資料
+app.get("/restaurants/:id", (req, res) => {
+  Restaurant.findById(req.params.id, (err, store) => {
+    if (err) return console.error(err);
+    return res.render("show", { store: store });
+  });
 });
 
+//搜尋餐廳  //要處理這個部分的詳細資料無法正常顯示
 app.get("/search", (req, res) => {
   const keyword = req.query.keyword;
-  const stores = restaurantList.results.filter(store => {
+  const restaurants = restaurantList.results.filter(store => {
     return store.name.includes(keyword) || store.category.includes(keyword);
   });
 
-  res.render("index", { restaurants: stores, keyword: keyword });
+  res.render("index", { restaurants: restaurants, keyword: keyword });
 });
+
+//新增餐廳的頁面
+app.get("/restaurant/new", (req, res) => {
+  res.render("new");
+});
+
+//把新增的餐廳加到資料庫
+app.post("/restaurants", (req, res) => {
+  const restaurant = Restaurant({
+    name: req.body.name,
+    name_en: req.body.name_en,
+    category: req.body.category,
+    image: req.body.image,
+    location: req.body.location,
+    phone: req.body.phone,
+    google_map: req.body.google_map,
+    rating: req.body.rating,
+    description: req.body.description
+  });
+
+  restaurant.save(err => {
+    if (err) return console.error(err);
+    return res.redirect("/");
+  });
+});
+
+//編輯餐廳的頁面
+app.get("/restaurants/:id/edit", (req, res) => {
+  Restaurant.findById(req.params.id, (err, store) => {
+    if (err) return console.error(err);
+    return res.render("edit", { store: store });
+  });
+});
+
+//將編輯好的內容儲存
+app.post("/restaurants/:id", (req, res) => {
+  Restaurant.findById(req.params.id, (err, restaurant) => {
+    console.log(req.params.id);
+    if (err) return console.error(err);
+    restaurant.name = req.body.name;
+    restaurant.name_en = req.body.name_en;
+    restaurant.category = req.body.category;
+    restaurant.image = req.body.image;
+    restaurant.location = req.body.location;
+    restaurant.phone = req.body.phone;
+    restaurant.google_map = req.body.google_map;
+    restaurant.rating = req.body.rating;
+    restaurant.description = req.body.description;
+
+    restaurant.save(err => {
+      if (err) return console.error(err);
+      return res.redirect(`/restaurants/${req.params.id}`);
+    });
+  });
+});
+
+//刪除餐廳
+app.post("/restaurants/:id/delete", (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`);
