9bd9fa6c74c72ad474b4b16c0b4b9e2bac0158a2
Walle0606 <michelle210181@gmail.com>
2020-07-05 12:53:18 +0800

    change seeder.js setting

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
 // 載入 Passport config
 require('./config/passport')(passport)
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/todo', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/restaurant', {
   useNewUrlParser: true,
   useCreateIndex: true,
 })
--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -1,33 +1,36 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-const Restaurant = require("../restaurant");
-const User = require("../user");
-const restaurantList = require("./restaurant.json").results;
-const userList = require("./user.json").results;
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const Restaurant = require('../restaurant')
+const User = require('../user')
+const restaurantList = require('./restaurant.json').results
+const userList = require('./user.json').results
 
-mongoose.connect("mongodb://localhost/restaurant", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/restaurant', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+})
 
-const db = mongoose.connection;
+const db = mongoose.connection
 
-db.on("error", () => {
-  console.log("db error");
-});
+db.on('error', () => {
+  console.log('db error')
+})
 
-db.once("open", () => {
-  console.log("db connected!");
+db.once('open', () => {
+  console.log('db connected!')
   for (let i = 0, count = 0; i < userList.length; i++) {
-    const user = User(userList[i]);
+    const user = User(userList[i])
     bcrypt.genSalt(10, (err, salt) => {
       bcrypt.hash(user.password, salt, (err, hash) => {
-        user.password = hash;
-        user.save();
-      });
-    });
+        user.password = hash
+        user.save()
+      })
+    })
 
     for (let j = i * 3; j < (i + 1) * 3; j++, count++) {
-      Restaurant.create({ ...restaurantList[j], userId: user._id });
-      if (count === restaurantList.length) return;
+      Restaurant.create({ ...restaurantList[j], userId: user._id })
+      if (count === restaurantList.length) return
     }
   }
-  console.log("done!");
-});
+  console.log('done!')
+})
